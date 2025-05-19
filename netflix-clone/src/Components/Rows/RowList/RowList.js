d4dc367aa71ef1d9c0766ff175b29d6ea1120aea
adismule <adismule@gmail.com>
2025-05-18 18:37:59 -0700

    updated

---
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Row from '../Row/Row'
+import requests from '../../../utils/requests'
+
+function RowList() {
+  return (
+    <>
+        <Row 
+        title = "NETFLIX ORIGINALS"
+        fetchUrl = {requests.fetchNetflixOriginals}
+        isLargeRow={true}
+        />
+        <Row title= "Trending Now" fetchUrl={requests.fetchTrending} />
+    </>
+  )
+}
+
+export default RowList
--- netflix-clone/src/utils/axios.js
+++ netflix-clone/src/utils/axios.js
@@ -1,16 +1,7 @@
-// import axios from 'axios';
-
-// const instance = axios.create({
-//   baseURL: 'https://api.themoviedb.org/3',
-// });
-
-// export default instance;
-
-// src/utils/axios.js
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://api.themoviedb.org/3', // ✅ correct
+  baseURL: 'https://api.themoviedb.org/3',
 });
 
 export default instance;
